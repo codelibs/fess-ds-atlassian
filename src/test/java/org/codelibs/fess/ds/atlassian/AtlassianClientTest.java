bf422028b4bb2d6e9939e9f708e020092e51fa04
Ryo Kuramoto <kuramoto@n2sm.net>
2018-08-01 14:52:24 +0900

    remake AtlassianClientTest

--- src/test/java/org/codelibs/fess/ds/atlassian/AtlassianClientTest.java
+++ src/test/java/org/codelibs/fess/ds/atlassian/AtlassianClientTest.java
@@ -15,32 +15,29 @@
  */
 package org.codelibs.fess.ds.atlassian;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 
 import com.google.api.client.http.apache.ApacheHttpTransport;
 
+import org.codelibs.fess.ds.atlassian.api.AtlassianClient;
+import org.codelibs.fess.ds.atlassian.api.AtlassianClientBuilder;
 import org.codelibs.fess.ds.atlassian.api.confluence.ConfluenceClient;
+import org.codelibs.fess.ds.atlassian.api.confluence.content.GetContentsRequest;
 import org.codelibs.fess.ds.atlassian.api.confluence.content.GetContentsResponse;
+import org.codelibs.fess.ds.atlassian.api.confluence.space.GetSpacesRequest;
 import org.codelibs.fess.ds.atlassian.api.confluence.space.GetSpacesResponse;
 import org.codelibs.fess.ds.atlassian.api.jira.JiraClient;
-import org.codelibs.fess.ds.atlassian.api.jira.JiraClientBuilder;
+import org.codelibs.fess.ds.atlassian.api.jira.project.GetProjectsRequest;
 import org.codelibs.fess.ds.atlassian.api.jira.project.GetProjectsResponse;
 import org.codelibs.fess.ds.atlassian.api.jira.search.SearchRequest;
 import org.codelibs.fess.ds.atlassian.api.jira.search.SearchResponse;
 import org.codelibs.fess.util.ComponentUtil;
 import org.dbflute.utflute.lastadi.ContainerTestCase;
-import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
 
 public class AtlassianClientTest extends ContainerTestCase {
-
-    public JiraClient jiraClient;
-    public ConfluenceClient confluenceClient;
 
     @Override
     protected String prepareConfigFile() {
@@ -55,16 +52,6 @@ public class AtlassianClientTest extends ContainerTestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-
-        jiraClient = JiraClient.builder().oAuthToken("", accessToken -> {
-            accessToken.consumerKey = "";
-            accessToken.signer = JiraClientBuilder.getOAuthRsaSigner("");
-            accessToken.transport = new ApacheHttpTransport();
-            accessToken.verifier = "";
-            accessToken.temporaryToken = "";
-        }).build();
-
-        confluenceClient = ConfluenceClient.builder().basicAuth("", "", "").build();
     }
 
     @Override
@@ -73,110 +60,233 @@ public class AtlassianClientTest extends ContainerTestCase {
         super.tearDown();
     }
 
-    public void test_get_projects() throws Exception {
-        System.out.println("-------- Test GetProjects --------");
-        final GetProjectsResponse response = jiraClient.getProjects().execute();
-        List<Map<String, Object>> projects = response.getProjects();
-        for (Map<String, Object> project : projects) {
-            System.out.println("name: " + project.get("name"));
+    public void test_production() {
+        // doProductionTest();
+    }
+
+    protected void doProductionTest() {
+        final JiraClient jiraClient = new JiraClient(AtlassianClient.builder().oAuthToken("", accessToken -> {
+            accessToken.consumerKey = "";
+            accessToken.signer = AtlassianClientBuilder.getOAuthRsaSigner("");
+            accessToken.transport = new ApacheHttpTransport();
+            accessToken.verifier = "";
+            accessToken.temporaryToken = "";
+        }).build());
+
+        final ConfluenceClient confluenceClient = new ConfluenceClient(AtlassianClient.builder().oAuthToken("", accessToken -> {
+            accessToken.consumerKey = "";
+            accessToken.signer = AtlassianClientBuilder.getOAuthRsaSigner("");
+            accessToken.transport = new ApacheHttpTransport();
+            accessToken.verifier = "";
+            accessToken.temporaryToken = "";
+        }).build());
+
+        doGetProjectsTest(jiraClient);
+        doSearchTest(jiraClient);
+        doGetContentsTest(confluenceClient);
+        doGetSpacesTest(confluenceClient);
+    }
+
+    protected void doGetProjectsTest(final JiraClient jiraClient) {
+        final GetProjectsResponse response = jiraClient.getProjects().expand("description").execute();
+        for (final Map<String, Object> project : response.getProjects()) {
+            assertTrue("not contains \"name\"", project.containsKey("name"));
+            assertTrue("not contains \"description\"", project.containsKey("description"));
         }
-        System.out.println("-------- Test GetProjects --------");
+    }
+
+    public void test_getProjects_fromJson() {
+        String json = "[{" + //
+                "    \"name\": \"Project-0\"" + //
+                "  }," + //
+                "  {" + //
+                "    \"name\": \"Project-1\"" + //
+                "  }" + //
+                "]";
+        final GetProjectsResponse response = GetProjectsRequest.fromJson(json);
+        final List<Map<String, Object>> projects = response.getProjects();
+        for (int i = 0; i < projects.size(); i++) {
+            final Map<String, Object> project = projects.get(i);
+            assertEquals(project.get("name"), "Project-" + i);
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public void test_search() throws Exception {
-        System.out.println("-------- Test Search --------");
-        final SearchResponse response = jiraClient.search().fields("summary", "description", "comment", "updated")
-                .execute();
-        for (Map<String, Object> issue : response.getIssues()) {
+    protected void doSearchTest(final JiraClient jiraClient) {
+        final SearchResponse response = jiraClient.search().fields("summary", "description", "comment", "updated").execute();
+        for (final Map<String, Object> issue : response.getIssues()) {
+            assertTrue(issue.containsKey("key"));
+            assertTrue("not contains \"fields\"", issue.containsKey("fields"));
             final Map<String, Object> fields = (Map<String, Object>) issue.get("fields");
-            System.out.println("summary: " + fields.get("summary"));
-            String desc = (String) fields.get("description");
-            System.out.println("description: " + desc.substring(0, Math.min(11, desc.length())));
-            List<Map<String, Object>> comments = (List<Map<String, Object>>) ((Map<String, Object>) fields
-                    .get("comment")).get("comments");
-            if (comments.size() > 0) {
-                System.out.println("comment:");
-                for (Map<String, Object> comment : comments) {
-                    String body = ((String) comment.get("body")).split("\n")[0];
-                    System.out.println(body.length() <= 10 ? body : (body.substring(0, 10) + "..."));
-                }
+            assertTrue("not contains \"summary\" in fields", fields.containsKey("summary"));
+            assertTrue("not contains \"description\" in fields", fields.containsKey("description"));
+            assertTrue("not contains \"comment\" in fields", fields.containsKey("comment"));
+            final Map<String, Object> commentObj = (Map<String, Object>) fields.get("comment");
+            final int totalComments = (int) commentObj.get("total");
+            final List<Map<String, Object>> comments = (List<Map<String, Object>>) commentObj.get("comments");
+            assertEquals(comments.size(), totalComments);
+            for (final Map<String, Object> comment : comments) {
+                assertTrue("not contains \"body\" in comment", comment.containsKey("body"));
             }
-            System.out.println("updated: "
-                    + new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX").parse((String) fields.get("updated")));
-            System.out.println("----");
+            assertTrue("not contains \"updated\" in fields", fields.containsKey("updated"));
+            final String updated = (String) fields.get("updated");
+            try {
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX").parse(updated);
+            } catch (final ParseException e) {
+                assertTrue("failed to parse \"updated\": " + updated, false);
+            }
         }
-        System.out.println("-------- Test Search --------");
     }
 
     @SuppressWarnings("unchecked")
-    public void test_search_from_json() throws Exception {
-        final String SUMMARY = "test_summary";
-        final String DESCRIPTION = "test_description";
-        final String COMMENT = "test_comment";
-        final String UPDATED = "2018-07-01T10:20:30.400+0000";
-
-        final XContentBuilder mappingBuilder = XContentFactory.jsonBuilder()//
-                .startObject().startArray("issues").startObject() //
-                .startObject("fields") //
-                .field("summary", SUMMARY) //
-                .field("description", DESCRIPTION) //
-                .startObject("comment") //
-                .startArray("comments").startObject() //
-                .field("body", COMMENT) //
-                .endObject().endArray() //
-                .endObject() //
-                .field("updated", UPDATED) //
-                .endObject() //
-                .endObject().endArray().endObject();
-        final String json = BytesReference.bytes(mappingBuilder).utf8ToString();
+    public void test_search_fromJson() {
+        final String json = "{" + //
+                "  \"total\": 2," + //
+                "  \"issues\": [{" + //
+                "      \"fields\": {" + //
+                "        \"summary\": \"Summary-0\"," + //
+                "        \"comment\": {" + //
+                "          \"total\": 2," + //
+                "          \"comments\": [" + //
+                "            { \"body\": \"Comment-0-0\" }," + //
+                "            { \"body\": \"Comment-0-1\" }" + //
+                "          ]" + //
+                "        }" + //
+                "      }," + //
+                "      \"key\": \"Key-0\"" + //
+                "    }," + //
+                "    {" + //
+                "      \"fields\": {" + //
+                "        \"summary\": \"Summary-1\"," + //
+                "        \"comment\": {" + //
+                "          \"total\": 0," + //
+                "          \"comments\": []" + //
+                "        }" + //
+                "      }," + //
+                "      \"key\": \"Key-1\"" + //
+                "    }" + //
+                "  ]" + //
+                "}";
         final SearchResponse response = SearchRequest.fromJson(json);
-        Map<String, Object> issue = response.getIssues().get(0);
-        final Map<String, Object> fields = (Map<String, Object>) issue.get("fields");
-
-        assertEquals(SUMMARY, fields.get("summary"));
-
-        assertEquals(DESCRIPTION, fields.get("description"));
-
-        List<Map<String, Object>> comments = (List<Map<String, Object>>) ((Map<String, Object>) fields.get("comment"))
-                .get("comments");
-        Map<String, Object> comment = comments.get(0);
-        String body = ((String) comment.get("body")).split("\n")[0];
-        assertEquals(COMMENT, body);
-
-        assertEquals(UPDATED, fields.get("updated"));
+        final List<Map<String, Object>> issues = response.getIssues();
+        for (int i = 0; i < issues.size(); i++) {
+            final Map<String, Object> issue = issues.get(i);
+            assertEquals(issue.get("key"), "Key-" + i);
+            final Map<String, Object> fields = (Map<String, Object>) issue.get("fields");
+            assertEquals(fields.get("summary"), "Summary-" + i);
+            final Map<String, Object> commentObj = (Map<String, Object>) fields.get("comment");
+            final int totalComments = (int) commentObj.get("total");
+            final List<Map<String, Object>> comments = (List<Map<String, Object>>) commentObj.get("comments");
+            assertEquals(comments.size(), totalComments);
+            for (int j = 0; j < comments.size(); j++) {
+                final Map<String, Object> comment = comments.get(j);
+                assertEquals(comment.get("body"), "Comment-" + i + "-" + j);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public void test_get_contents() throws Exception {
-        System.out.println("-------- Test GetContents --------");
-        final GetContentsResponse response = confluenceClient.getContents().expand("space", "version", "body.view")
-                .execute();
-        for (Map<String, Object> content : response.getContents()) {
-            System.out.println("id: " + content.get("id"));
-            System.out.println("type: " + content.get("type"));
-            System.out.println("title: " + content.get("title"));
-            Map<String, Object> version = (Map<String, Object>) content.get("version");
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
-            format.setTimeZone(TimeZone.getTimeZone("UTC"));
-            Date date = format.parse((String) version.get("when"));
-            System.out.println("lastUpdated: " + date);
-            Map<String, Object> bodyObj = (Map<String, Object>) content.get("body");
-            String body = (String) ((Map<String, Object>) bodyObj.get("view")).get("value");
-            body = body.split("\n")[0];
-            System.out.println(body.length() <= 10 ? body : (body.substring(0, 10) + "..."));
-            System.out.println("----");
+    protected void doGetContentsTest(final ConfluenceClient confluenceClient) {
+        final GetContentsResponse response =
+                confluenceClient.getContents().expand("body.view", "children.comment.body.view", "version").execute();
+        for (final Map<String, Object> content : response.getContents()) {
+            assertTrue("not contains \"title\"", content.containsKey("title"));
+
+            assertTrue("not contains \"body\"", content.containsKey("body"));
+            final Map<String, Object> body = (Map<String, Object>) content.get("body");
+            assertTrue("not contains \"view\" in \"body\"", body.containsKey("view"));
+            final Map<String, Object> view = (Map<String, Object>) body.get("view");
+            assertTrue("not contains \"value\" in \"body.view\"", view.containsKey("value"));
+
+            assertTrue("not contains \"children\"", content.containsKey("children"));
+            final Map<String, Object> children = (Map<String, Object>) content.get("children");
+            assertTrue("not contains \"comment\" in \"children\"", children.containsKey("comment"));
+            final List<Map<String, Object>> comments =
+                    (List<Map<String, Object>>) ((Map<String, Object>) children.get("comment")).get("results");
+            for (final Map<String, Object> comment : comments) {
+                assertTrue("not contains \"body\" in \"children.comment\"", comment.containsKey("body"));
+                final Map<String, Object> commentBody = (Map<String, Object>) content.get("body");
+                assertTrue("not contains \"view\" in \"children.comment.body\"", commentBody.containsKey("view"));
+                final Map<String, Object> commentView = (Map<String, Object>) commentBody.get("view");
+                assertTrue("not contains \"value\" in \"children.comment.body.view\"", commentView.containsKey("value"));
+            }
+
+            assertTrue("not contains \"version\"", content.containsKey("version"));
+            final Map<String, Object> version = (Map<String, Object>) content.get("version");
+            assertTrue("not contains \"when\" in \"version\"", version.containsKey("when"));
+            final String when = (String) version.get("when");
+            try {
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX").parse(when);
+            } catch (final ParseException e) {
+                assertTrue("failed to parse \"when\": " + when, false);
+            }
         }
-        System.out.println("-------- Test GetContents --------");
     }
 
-    public void test_get_spaces() throws Exception {
-        System.out.println("-------- Test GetSpaces --------");
-        final GetSpacesResponse response = confluenceClient.getSpaces().execute();
-        for (Map<String, Object> space : response.getSpaces()) {
-            System.out.println("name: " + space.get("name"));
+    @SuppressWarnings("unchecked")
+    public void test_getContents_fromJson() {
+        final String json = "{" + //
+                "  \"results\": [{" + //
+                "      \"title\": \"Title-0\"," + //
+                "      \"body\": { \"view\": { \"value\": \"Body-0\" } }," + //
+                "      \"children\": {" + //
+                "        \"comment\": {" + //
+                "          \"size\": 1," + //
+                "          \"results\": [{" + //
+                "            \"title\": \"CommentTitle-0-0\"," + //
+                "            \"body\": { \"view\": { \"value\": \"CommentBody-0-0\" } }" + //
+                "          }]" + //
+                "        }" + //
+                "      }," + //
+                "      \"version\": { \"when\": \"2018-08-01T12:34:56.789Z\" }" + //
+                "    }," + //
+                "    {" + //
+                "      \"title\": \"Title-1\"," + //
+                "      \"body\": { \"view\": { \"value\": \"Body-1\" } }," + //
+                "      \"children\": {" + //
+                "        \"comment\": {" + //
+                "          \"size\": 0," + //
+                "          \"results\": []" + //
+                "        }" + //
+                "      }," + //
+                "      \"version\": { \"when\": \"2018-08-01T12:34:56.789Z\" }" + //
+                "    }" + //
+                "  ]" + //
+                "}";
+        final GetContentsResponse response = GetContentsRequest.fromJson(json);
+        final List<Map<String, Object>> contents = response.getContents();
+        for (int i = 0; i < contents.size(); i++) {
+            final Map<String, Object> content = contents.get(i);
+            assertEquals(content.get("title"), "Title-" + i);
+            final Map<String, Object> body = (Map<String, Object>) content.get("body");
+            final Map<String, Object> view = (Map<String, Object>) body.get("view");
+            assertEquals(view.get("value"), "Body-" + i);
+            final Map<String, Object> version = (Map<String, Object>) content.get("version");
+            assertEquals(version.get("when"), "2018-08-01T12:34:56.789Z");
         }
-        System.out.println("-------- Test GetSpaces --------");
+    }
+
+    protected void doGetSpacesTest(final ConfluenceClient confluenceClient) {
+        final GetSpacesResponse response = confluenceClient.getSpaces().expand("description").execute();
+        for (final Map<String, Object> space : response.getSpaces()) {
+            assertTrue("not contains \"name\"", space.containsKey("name"));
+            assertTrue("not contains \"description\"", space.containsKey("description"));
+        }
+    }
+
+    public void test_getSpaces_fromJson() {
+        String json = "{" + //
+                "  \"results\": [" + //
+                "    { \"name\": \"Space-0\" }," + //
+                "    { \"name\": \"Space-1\" }" + //
+                "  ]" + //
+                "}";
+        final GetSpacesResponse response = GetSpacesRequest.fromJson(json);
+        final List<Map<String, Object>> spaces = response.getSpaces();
+        for (int i = 0; i < spaces.size(); i++) {
+            final Map<String, Object> space = spaces.get(i);
+            assertEquals(space.get("name"), "Space-" + i);
+        }
     }
 
 }
