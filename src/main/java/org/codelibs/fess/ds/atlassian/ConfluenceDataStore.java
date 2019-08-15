d3c47d184a6750df41d72a5674803a60bc57ae52
Shinsuke Sugaya <shinsuke@apache.org>
2019-08-15 17:49:42 +0900

    fess 13.3

--- src/main/java/org/codelibs/fess/ds/atlassian/ConfluenceDataStore.java
+++ src/main/java/org/codelibs/fess/ds/atlassian/ConfluenceDataStore.java
@@ -70,7 +70,7 @@ public class ConfluenceDataStore extends AbstractDataStore {
     protected Extractor extractor;
 
     protected String getName() {
-        return "Confluence";
+        return this.getClass().getSimpleName();
     }
 
     @Override
